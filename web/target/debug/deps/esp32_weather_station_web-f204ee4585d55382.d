def8e6fc444970b579fa91ec270ff5da419823ad
connorcinna <connorcinna@gmail.com>
2024-04-27 13:34:33 -0400

    added web server and simple web page

--- web/target/debug/deps/esp32_weather_station_web-f204ee4585d55382.d
+++
@@ -1,5 +0,0 @@
-/home/connor/projects/esp32-weather-station/web/target/debug/deps/esp32_weather_station_web-f204ee4585d55382: src/main.rs
-
-/home/connor/projects/esp32-weather-station/web/target/debug/deps/esp32_weather_station_web-f204ee4585d55382.d: src/main.rs
-
-src/main.rs:
--- web/target/debug/esp32-weather-station-web.d
+++
@@ -1,1 +0,0 @@
-/home/connor/projects/esp32-weather-station/web/target/debug/esp32-weather-station-web: /home/connor/projects/esp32-weather-station/web/src/main.rs
